79f74471570e8aedec686ee060512c41b500a141
adarsh-ks2503 <adarshphpdevs@gmail.com>
2025-05-23 13:45:32 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 
 export default function RootLayout({
   children,
